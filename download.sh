fbeadc811afccdd1a0ed564d1f05995eacf6f830
Kong Kao <kong0107@gmail.com>
2016-03-03 21:15:31 +0800

    Update download.sh: overwrite existing source/*.xml

--- download.sh
+++ download.sh
@@ -8,13 +8,13 @@ cd source
 git init
 
 curl -L -o FalVMingLing.zip "http://law.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=CFM"
-unzip FalVMingLing.zip
+unzip -o FalVMingLing.zip
 
 curl -L -o Eng_FalVMingLing.zip "http://law.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=EFM"
-unzip Eng_FalVMingLing.zip
+unzip -o Eng_FalVMingLing.zip
 
 curl -L -o HisMingLing.zip "http://law.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=CHM"
-unzip HisMingLing.zip
+unzip -o HisMingLing.zip
 
 rm *.zip
 git add .
