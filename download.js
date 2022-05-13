676b1a0ae6b73300024d5ca079a576409eaa5dc1
kong0107 <kong0107@gmail.com>
2022-05-13 15:41:41 +0800

    下載時自動判斷是否為新版

--- download.js
+++ download.js
@@ -25,15 +25,33 @@ async function downloadAndUnzip(url, absPath) {
 
 async function main() {
     await downloadAndUnzip(
-        'https://sendlaw.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=CFM',
-        __dirname + '/source/'
-    );
-    await downloadAndUnzip(
         'https://sendlaw.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=EFM',
         __dirname + '/source/'
     );
-    await fsP.unlink('./source/schema.csv');
-    await fsP.unlink('./source/manifest.csv');
+
+    const xml = await fsP.readFile('./source/Eng_FalVMingLing.xml', 'utf8');
+    const match = xml.match(/UpdateDate="(\d{4})\/?(\d{1,2})\/?(\d{1,2})/);
+    if(match[2].length !== 2) match[2] = '0' + match[2];
+    if(match[3].length !== 2) match[3] = '0' + match[3];
+    const newDate = match[1] + match[2] + match[3];
+    process.stdout.write(`UpdateDate: ${newDate}\n`);
+
+    let oldDate = '';
+    try {
+        oldDate = await fsP.readFile('./xml/UpdateDate.txt', 'utf-8');
+    }
+    catch(e) {}
+
+    if(newDate > oldDate) {
+        await downloadAndUnzip(
+            'https://sendlaw.moj.gov.tw/PublicData/GetFile.ashx?DType=XML&AuData=CFM',
+            __dirname + '/source/'
+        );
+    }
+    else process.stdout.write('data not updated yet\n');
+
+    fsP.unlink('./source/schema.csv');
+    fsP.unlink('./source/manifest.csv');
 }
 
 if(module && module.parent) module.exports = main;
