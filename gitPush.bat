3819fc84026d0bee797c385721e89f3a8fa785b2
kong0107 <kong0107@gmail.com>
2022-10-05 17:48:26 +0800

    fix punctuations in articles

--- gitPush.bat
+++ gitPush.bat
@@ -1,20 +1,4 @@
 @echo off
-
-cd json_split
-git add .
-git commit -m "UpdateDate 20220923"
-git tag 20220923_swagger
-git push
-git push --tags
-cd ..
-
-cd json_arrange
-git add .
-git commit -m "UpdateDate 20220923"
-git tag 20220923_arrange
-git push
-git push --tags
-cd ..
-
+echo "No updates to push."
 pause
 @echo on
