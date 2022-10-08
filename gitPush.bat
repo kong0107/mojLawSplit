f013ccae9eb0697ca94e960dacb2730b4bbc158e
kong0107 <kong0107@gmail.com>
2022-10-09 01:50:37 +0800

    nested article contents

--- gitPush.bat
+++ gitPush.bat
@@ -1,4 +1,20 @@
 @echo off
-echo "No updates to push."
+
+cd json_split
+git add .
+git commit -m "UpdateDate 20220930"
+git tag 20220930_swagger
+git push
+git push --tags
+cd ..
+
+cd json_arrange
+git add .
+git commit -m "UpdateDate 20220930"
+git tag 20220930_arrange
+git push
+git push --tags
+cd ..
+
 pause
 @echo on
